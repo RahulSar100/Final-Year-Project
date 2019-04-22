34cdb1a39dfb00363430c2c0bcdb7510fb2b1c64
RahulSar100 <43545594+RahulSar100@users.noreply.github.com>
2019-04-22 19:45:25 +0530

    Update register.php
    new register php file

--- register.php
+++ register.php
@@ -15,10 +15,23 @@ $gender=$_REQUEST['gender'];
 
 $sql = "INSERT INTO user_register (First_Name,Middle_Name,Last_Name,DoB,Gender,Email,Mobile_no,Password,Con_Password) VALUES ('$fname','$mname','$lname','$dob','$gender','$email','$mobile','$password','$conf_password')";
 
-if(mysqli_query($conn,$sql)) {
-echo '<script type="text/javascript">alert("Congratuation. You are now registered.")</script>';
-//header("location:home.html");    
-}
+if(mysqli_query($conn,$sql))
+{
+ echo '<script type="text/javascript">alert("Congratuation. You are now registered.")</script>';
+ //now directing the user to the home.php
+ $sql1 = "SELECT * FROM user_register WHERE Email='$email' and Password='$password'";
+ $result1 = mysqli_query($conn,$sql1) or die(mysqli_error($conn));
+
+ if (mysqli_num_rows($result1)>0)
+  {
+   session_start();
+   $row1=mysqli_fetch_array($result1);
+   	$_SESSION['id'] = $row['user_id'];
+   $_SESSION['username'] = $row1['First_Name'];
+   $_SESSION['loggedin'] = true;
+   header("Location: home.php?msg=Account created and login succefully");  //if succeful then to homepage
+  }
+ }
 else{
 	die("Error in inserting data into database".mysqli_error($conn));
 }
