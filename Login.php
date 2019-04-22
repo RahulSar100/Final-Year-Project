77fce380025f5c88da65e170b02b30b190ae624e
RahulSar100 <43545594+RahulSar100@users.noreply.github.com>
2019-04-22 19:35:15 +0530

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,22 +1,23 @@
 <?php
 $conn=mysqli_connect('localhost','root','','mha') or die(mysqli_connect_error());
 
-$username=$_REQUEST['username']; 
-$password=$_REQUEST['pass']; 
-$sql = "SELECT * FROM user_register WHERE Email='$username' and Password='$pass'";
+$email=$_REQUEST['email'];
+$password=$_REQUEST['pass'];
+$sql = "SELECT * FROM user_register WHERE Email='$email' and Password='$password'";
 $result=mysqli_query($conn,$sql) or die(mysqli_error($conn));
 
 if (mysqli_num_rows($result)>0)
 {
 	session_start();
 	$row=mysqli_fetch_array($result);
-	$_SESSION['username'] = $row['name'];
-    $_SESSION['loggedin'] = true;
-	
-  header("Location: home.html?msg=Login_Sucessfull");  //if succeful then to homepage
+	$_SESSION['id'] = $row['user_id'];
+	$_SESSION['username'] = $row['First_Name'];
+  $_SESSION['loggedin'] = true;
+
+  header("Location: home.php?msg=Login_Sucessfull");  //if succeful then to homepage
 }
 else{
 	header("Location: Login.html?msg=Login_Failed");   //again to login page
-} 
+}
 mysqli_close($conn);
 ?>
